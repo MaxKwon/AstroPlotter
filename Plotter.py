e353434c0b752a63a46cc98f969ea9f4d27391fb
Max Kwon <maxkwonusa@yahoo.com>
2018-07-17 07:22:05 -1000

    some upgrades to plotting

--- Plotter.py
+++ Plotter.py
@@ -12,11 +12,14 @@ from astropy.io import fits
 from tkinter.filedialog import askopenfilename
 from tkinter import *
 import math
+import astroquery.simbad #using to fill blank data points with queries from simbad database
 import re #the regular expressions package for detecting input format
 
 class Plotter():
     
     def __init__(self):
+        
+        self.test = False
         
         self.filename = askopenfilename(filetypes=[("Text Files", "*.txt")], title='Data')
         self.fig = plt.figure(figsize=(8,6))
@@ -25,19 +28,19 @@ class Plotter():
         self.form = self.detFormat()
         print(self.form)
         
-        
-    def detFormat(self):
+    def detFormat(self): #BROKEN
         
         with open(self.filename) as f:
             content = f.readlines()
     
-        print (content[1]) # second row of the data
-        
+        if (self.test):
+            print (content[1]) # first actual row of the data content[0] is the headers
+              
         regex = []
-        regex.append('\d{1,2}[h]\d{1,2}[m][+ -]{,1}\d{1,2}[d]\d{1,2}[m]')
-        regex.append('\d{1,2}[h]\d{1,2}[+ -]{,1}\d{1,2}[d]\d{1,2}')
-        regex.append('\d*\s*\d*[.]{,1}\d*\s*\d{1,3}\s\d{1,3}\s\d{1,3}[.]{,1}\d{,3}\s[+-]{,1}\d{1,3}\s\d{1,3}\s\d{1,3}[.]{,1}\d{,3}') #this will work with rachels now with the addition of '\s' (whitespace in general)
-        regex.append('\d{1,3}:\d{1,3}:\d{1,3}[.]{,1}\d{,4}[+-]{,1}\d{1,3}:\d{1,3}:\d{1,3}[.]{,1}\d{,4}')
+        regex.append('\d{1,2}[h]\d{1,2}[m][+ -]{,1}\d{1,2}[d]\d{1,2}[m]') #15h17m-11d10m
+        regex.append('\d{1,2}[h]\d{1,2}[+ -]{,1}\d{1,2}[d]\d{1,2}') #15h17+89d15
+        regex.append('\d*\s*\d*[.]{,1}\d*\s*\d{1,3}\s\d{1,3}\s\d{1,3}[.]{,1}\d{,3}\s[+-]{,1}\d{1,3}\s\d{1,3}\s\d{1,3}[.]{,1}\d{,3}') #20 54 05.689 +37 01 17.38 
+        regex.append('\d{1,3}:\d{1,3}:\d{1,3}[.]{,1}\d{,4}[+-]{,1}\d{1,3}:\d{1,3}:\d{1,3}[.]{,1}\d{,4}') # 10:12:45.3-45:17:50
           
         format_index = 9
              
@@ -57,34 +60,57 @@ class Plotter():
             form = "c"
         elif (format_index == 3):
             form = "d"
-        #FINISH THE OUTPUT, DETERMINE TYPE BASED ON FORMAT_INDEX!!!
-        
-        #some sort of determination
-        #using regex libraries to determine the format of the input for processing 
         
         return form
+    
+    def formatData(self, form):
+        
+        data = ascii.read(self.filename)
+        
+        ras = []
+        decs = []
+        
+        if (form == "c"): 
+            for i in range(len(data)):
+                ra_str = str(data[i][2]) + ":" + str(data[i][3]) + ":" + str(data[i][4])
+                dec_str = str(data[i][5]) + ":" + str(data[i][6]) + ":" + str(data[i][7])
+                
+                ra = coord.Angle(ra_str, unit=u.hour)
+                dec = coord.Angle(dec_str, unit=u.degree)
+                ra = ra.wrap_at(12 * u.hourangle)
+ 
+                ras.append(ra.radian)
+                decs.append(dec.radian)
+        
+        return ras, decs
         
     def getFile(self):
         
         self.filename = askopenfilename(filetypes=[("Text Files", "*.txt")], title='Data')
         
-    def readFileData(self):
+    def printFileData(self): #BROKEN
         
-        data = ascii.read(self.filename, delimiter = "\s", header_start=0, data_start=1)
-        #data['EPICID'].fill_value = "N/A"
-        print (data)#.filled())
+        #Maybe take this stuff out i dont know, need it to know delimination or something I guess
+        if (self.form == "c"):
+            del_ = "\s"
+            d_s = 1
+        elif (self.form == "a" or self.form == "b"):
+            del_ = "\s"
+            d_s = 0
+            
+        
+        data = ascii.read(self.filename, delimiter = del_, data_start=d_s)
+        print (data)
+        print (type(data))
+            
+        
+    def plotFileData(self): 
+    
+        ras, decs = self.formatData(self.form)
+        
+        self.ax.scatter(ras, decs)
         
         
-    def plotFileData(self):
-        
-        data = ascii.read(self.filename, delimiter = "\s", header_start=0, data_start=1)
-        data['ra'].fill_value = "N/A"
-        
-        ra = coord.Angle(data['ra']*u.degree)
-        ra = ra.wrap_at(180*u.degree)
-        dec = coord.Angle(data['dec']*u.degree)
-        
-        self.ax.scatter(ra.radian, dec.radian)
         
     def plotCelEq(self):
         
@@ -131,11 +157,36 @@ class Plotter():
             y_plot.append(y)
             
         self.ax.plot(longitude2,latitude2,'g-')
-
+        
+    def plotTelescopeLimit(self):
+        
+        #limits for the gemini north telescope 
+        dec_lim = [-30, 73] #degrees
+        ra_lim = [105, 345] #degrees
+        
+        ra = coord.Angle(ra_lim*u.degree)
+        ra = ra.wrap_at(180*u.degree)
+        dec = coord.Angle(dec_lim*u.degree)
+        
+        #maybe clean this up 
+        self.ax.plot([ra[0].radian, ra[0].radian], [dec[0].radian, dec[1].radian], "k-")
+        self.ax.plot([ra[0].radian, ra[1].radian], [dec[1].radian, dec[1].radian], "k-")
+        self.ax.plot([ra[1].radian, ra[1].radian], [dec[1].radian, dec[0].radian], "k-")
+        self.ax.plot([ra[1].radian, ra[0].radian], [dec[0].radian, dec[0].radian], "k-")
+        
+    def fullPlot(self):
+        
+        self.plotCelEq()
+        self.plotTelescopeLimit()
+        self.plotFileData()
+        
+        
+    
 plotter = Plotter()
-plotter.readFileData()
-
-
+#plotter.plotTelescopeLimit()
+#plotter.plotCelEq()
+#plotter.printFileData()
+plotter.fullPlot()
 
 
 
---
+++ TestSimpleIn.py
@@ -0,0 +1,47 @@
+"""
+Created on Wed Jun 26 10:51:03 2018
+@author: Max K. Kwon 
+"""
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from astropy.io import ascii
+import astropy.coordinates as coord
+import astropy.units as u
+from astropy.io import fits
+from tkinter.filedialog import askopenfilename
+from tkinter import *
+import math
+from astroquery.simbad import Simbad
+import re #the regular expressions package for detecting input format
+
+
+filename = askopenfilename(filetypes=[("Text Files", "*.txt")], title='Data')
+file = open(filename)
+data = file.read().splitlines()
+
+
+#This is hella slow, there is probably a way to do this matrixy? 
+#make sure that when this is implemented, that its in a try, should be able to work without internet connection 
+
+ras = []
+decs = []
+
+print(len(data))
+for i in range(len(data)):
+ 
+    print(i)
+    result_table = Simbad.query_object(data[i])
+    print(len(result_table))
+ 
+    ra = coord.Angle(result_table["RA"][0], unit=u.hour)
+    dec = coord.Angle(result_table["DEC"][0], unit=u.degree)
+    ra = ra.wrap_at(12 * u.hourangle)
+ 
+    ras.append(ra.radian)
+    decs.append(dec.radian)
+ 
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection="mollweide")
+ax.scatter(ras, decs)
+
